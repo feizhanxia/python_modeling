48284fec81d7b0e063a6769c2c4483ce03465d5c
feizhanxia <2050570@tongji.edu.cn>
2022-08-30 10:15:36 +0800

    first class 一些例子

--- 盲人爬山.py
+++ 盲人爬山.py
@@ -1,10 +1,16 @@
 import numpy as np
 import numpy.random as R
+R.seed(29)
+
+# 学习速率
+lr = 1
 startx = R.random()
 starty = np.sin(startx)
 for i in range(100):
+    if (i+1)%10 == 0:
+        lr /= 5
     delta = R.random() - 0.5
-    x1 = startx + delta
+    x1 = startx + delta*lr
     y1 = np.sin(x1)
     if y1 > starty:
         startx = x1
